e9302f0b7ca285c03833100e37177d1809996ba3
vladimirStary <starovoyt_1995@mail.ru>
2023-02-11 11:22:07 +0300

    Подчищен мусор

--- GeometryCalculator/Figure.cs
+++ GeometryCalculator/Figure.cs
@@ -8,10 +8,6 @@ namespace GeometryCalculator
 {
     public abstract class Figure
     {
-        private readonly Lazy<double> _square;
-
-        public double Square => _square.Value;
-
         public double GetSquare()
         {
             return new Lazy<double>(CalculateSquare()).Value;
